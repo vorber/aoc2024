5eed3e5ccf14ed841d9f4009b7401f72479d0fe0
vorber <vorber@gmail.com>
2024-12-15 19:33:30 +0200

    day 15 refactor

--- src/puzzles/day15.rs
+++ src/puzzles/day15.rs
@@ -27,25 +27,31 @@ fn parse_input(input:&String) -> (Grid<char>, Vec<Point>) {
 
 fn part1(grid:&Grid<char>, moves:&Vec<Point>) -> usize {
     let mut g = Grid {width: grid.width, height: grid.height, cells: grid.cells.clone()};
+    _solve(&mut g, moves)
+}
+
+fn part2(grid:&Grid<char>, moves:&Vec<Point>) -> usize {
+    let mut g = extend_grid_p2(grid);
+    _solve(&mut g, moves)
+}
+
+fn _solve(g: &mut Grid<char>, moves:&Vec<Point>) -> usize {
     let mut r = g.find('@').expect("Should have robot");
     for m in moves {
-        let mut n = r+*m;
-        while Some(&'O') == g.try_get(n) { n += *m; }
-        match g.try_get(n) {
-            Some('.') => {
-                while n != r { g[&n] = g[&n-m]; n -= *m; }
-                g[&r] = '.';
-                r = r+*m;
-            },
-            Some('#') |  None => { continue; }
-            _ => { unreachable!() }
+        match try_move(r, *m, &g) {
+            Ok(to_move) => {
+                to_move.iter().for_each(|(_, from, _)| g[from] = '.');
+                to_move.iter().for_each(|(to, _, v)| g[to] = *v);
+                r += *m;
+            }
+            Err(_) => continue
         }
     }
     score(&g)
 }
 
-fn part2(grid:&Grid<char>, moves:&Vec<Point>) -> usize {
-    let mut g = Grid {
+fn extend_grid_p2(grid:&Grid<char>) -> Grid<char> {
+    Grid {
         width: grid.width*2, 
         height: grid.height, 
         cells: grid.cells.clone().into_iter().flat_map(|c| match c {
@@ -55,59 +61,38 @@ fn part2(grid:&Grid<char>, moves:&Vec<Point>) -> usize {
             '#' => ['#', '#'],
             _ => unreachable!()
         }).collect_vec()
-    };
-    let mut r = g.find('@').expect("Should have robot");
-    for m in moves {
-        if let Some(c) = cells_to_move(&r, &m, &g) {
-            let new = c.iter()
-                .map(|p| (p, p+m, g[p]))
-                .collect_vec();
-            new.iter().for_each(|(p,n,v)| g[*p] = '.');
-            new.iter().for_each(|(p,n,v)| g[n] = *v);
-            g[&r] = '.';
-            r += *m;
-            g[&r] = '@';
-        }
     }
-    score(&g)
 }
 
-fn cells_to_move(from: &Point, dir: &Point, grid:&Grid<char>) -> Option<Vec<Point>> {
-    if dir == &EAST || dir == &WEST {
-        let mut n = 0;
-        while let Some(v) = grid.try_get(from.offset(n, *dir)) {
-            match v {
-                '@' | '[' | ']' => n += 1,
-                '.' => break,
-                '#' => return None,
-                _ => unreachable!()
+enum CantMoveReason {
+    OutOfBounds,
+    Wall
+}
+
+fn try_move(from: Point, dir: Point, grid:&Grid<char>) -> Result<Vec<(Point, Point, char)>,CantMoveReason> {
+    let to = from+dir;
+    let mut result = vec![(to, from, grid[from])];
+    let v = grid.try_get(to);
+    match v {
+        None => Err(CantMoveReason::OutOfBounds),
+        Some('.') => Ok(result),
+        Some('#') => Err(CantMoveReason::Wall),
+        Some(c@'[') | Some(c@']') | Some(c@'O') => {
+            match dir {
+                EAST | WEST => { result.extend(try_move(to, dir, grid)?); Ok(result) }
+                _ => {
+                    let other = match c {
+                        '[' => try_move(to+EAST, dir, grid)?,
+                        ']' => try_move(to+WEST, dir, grid)?,
+                        _ => Vec::new()
+                    };
+                    result.extend(other);
+                    result.extend(try_move(to, dir, grid)?);
+                    Ok(result)
+                } 
             }
-        }
-        return Some((1..n).map(|i| from.offset(i, *dir)).collect_vec())
-    } else {
-        match grid.try_get(from) {
-            None | Some('#') => return None,
-            Some('.') => { return Some(Vec::new()); },
-            Some('@') | Some('[') | Some(']') => {
-                let n = from + dir;
-                match grid.try_get(n) {
-                    None | Some('#') => return None,
-                    Some('.') => { return Some(Vec::new()); }
-                    Some('[') => {
-                        let c1 = cells_to_move(&n, dir, grid)?;
-                        let c2 = cells_to_move(&(n + EAST), dir, grid)?;
-                        return Some(vec![vec![n, n+EAST],c1,c2].into_iter().flatten().collect_vec());
-                    },
-                    Some(']') => {
-                        let c1 = cells_to_move(&n, dir, grid)?;
-                        let c2 = cells_to_move(&(n+WEST), dir, grid)?;
-                        return Some(vec![vec![n, n+WEST],c1,c2].into_iter().flatten().collect_vec());
-                    },
-                    _ => unreachable!()
-                }
-            }
-            _ => unreachable!()
-        }
+        },
+        _ => unreachable!()
     }
 }
 
